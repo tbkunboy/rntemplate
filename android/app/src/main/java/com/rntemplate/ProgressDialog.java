01b36bd7e2b02238a85064921f8b03bb4210b10c
ngo duy nhat <ngonhat1094@gmail.com>
2021-01-19 13:47:21 +0700

    init commit

--- android/app/src/main/java/com/rntemplate/ProgressDialog.java
+++ android/app/src/main/java/com/rntemplate/ProgressDialog.java
@@ -1,4 +1,55 @@
 package com.rntemplate;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ProgressBar;
+
+import vn.jupviec.jupviec.R;
+
 public class ProgressDialog {
+
+    private static Dialog dialog;
+
+    public static void showLoading(final Activity activity) {
+        if (activity != null) {
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (dialog != null && dialog.isShowing()) {
+                        dialog.dismiss();
+                        dialog = null;
+                    }
+
+                    dialog = new Dialog(activity, R.style.ProgressDialog);
+                    dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                    dialog.setContentView(R.layout.layout_progress_dialog);
+
+                    Animation animation = AnimationUtils.loadAnimation(activity.getApplicationContext(), R.anim.rotate);
+                    ProgressBar progressBar = (ProgressBar) dialog.findViewById(R.id.circular_progress_bar);
+                    progressBar.startAnimation(animation);
+
+                    if (dialog != null && !dialog.isShowing()) {
+                        dialog.show();
+                    }
+                }
+            });
+        }
+    }
+
+    public static void hideLoading(final Activity activity) {
+        if (activity != null) {
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (dialog != null && dialog.isShowing()) {
+                        dialog.dismiss();
+                        dialog = null;
+                    }
+                }
+            });
+        }
+    }
 }
---
+++ src/navigation/AppNavigation.js
@@ -0,0 +1,8 @@
+import {NavigationContainer} from '@react-navigation/native';
+
+export default AppNavigation = () => {
+    return(
+    <NavigationContainer>
+
+    </NavigationContainer>)
+};
